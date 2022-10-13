b355c3d8a45cbb94d24c3b4b9b79136c43973198
apedronc <112122277+apedronc@users.noreply.github.com>
2022-10-12 22:13:27 -0400

    Completes ERxercise

--- UI/HUD.gd
+++ UI/HUD.gd
@@ -23,8 +23,13 @@ func update_score():
 func update_time():
 	$Time.text = "Time: " + str(Global.time)
 
+	
 func update_fever():
 	$Fever.value = Global.fever
+	var styleBox = $Fever.get("custom_styles/fg")
+	styleBox.bg_color.h = fever_h
+	styleBox.bg_color.s = (Global.fever / 100.0) * fever_s
+	styleBox.bg_color.v = (fever_v/2) + ((Global.fever / 100.0) * (fever_v/2)) 
 
 func update_lives():
 	var indicator_pos = Vector2(indicator_margin.x, Global.VP.y - indicator_margin.y)
